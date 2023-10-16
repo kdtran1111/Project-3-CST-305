94012f58abb3b064e9f70cd3e6cfd89ca57cd2a3
kdtran1111 <134137323+kdtran1111@users.noreply.github.com>
2023-10-16 03:25:39 -0700

    Add files via upload

---
+++ README (2).md
@@ -0,0 +1,13 @@
+Install numpy, matplotlib and jupyter notebook if not already have them
+
+Open the program in jupyter notebook
+ 
+The program is to solve and plot the ODEs result for:
+1. 𝑦"+4𝑦=𝑥;𝑦(0)=𝑦′(0)=0
+
+2. 𝑦"+𝑦=4; 𝑦(0)=𝑦′(0)=0
+
+Run the program on Jupyter notebook
+
+
+After executing, the graph will show and the execution time will be printed.
---
+++ Untitled3.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the functions
+def f1(x):
+    return (-1/8) * np.sin(2 * x) + (1/4) * x
+
+def f2(x):
+    return 4 * (1 - np.cos(x))
+
+# Generate x values
+x = np.linspace(-10, 10, 400)
+
+# Calculate y values for both functions
+y1 = f1(x)
+y2 = f2(x)
+
+# Plot the functions
+plt.plot(x, y1, label="ODE 1")
+plt.plot(x, y2, label="ODE 2", linestyle='--')
+
+# Setting the title and labels
+plt.title("Plot of the given functions")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+#Plotting the Green's function result
+
+# Generate t values
+t = np.linspace(-10, 10, 400)  # Generates 1000 points between 0 and 10
+
+# Calculate the y values for each function
+y1 = (-1/8)*np.sin(2*t) + (1/4)*t
+y2 = 4*(1-np.cos(t))
+
+# Plot the first function
+plt.plot(t, y1, label='ODE 1 Green')
+
+# Plot the second function
+plt.plot(t, y2, label='ODE 2 Green')
+
+# Set title and labels
+plt.title('Plot of the given functions')
+plt.xlabel('t')
+plt.ylabel('y')
+
+# Show legend
+plt.legend()
+
+# Display the plot
+plt.grid(True)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
